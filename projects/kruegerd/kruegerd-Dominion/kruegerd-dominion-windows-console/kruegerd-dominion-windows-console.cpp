a830895c52135aec149a4efdc81098d0c5123f6b
Daniel Krueger <28245241+dwkrueger2@users.noreply.github.com>
2018-10-27 03:07:19 -0400

    Modified code to call cardEffectAdventure to the new signature

--- projects/kruegerd/kruegerd-Dominion/kruegerd-dominion-windows-console/kruegerd-dominion-windows-console.cpp
+++ projects/kruegerd/kruegerd-Dominion/kruegerd-dominion-windows-console/kruegerd-dominion-windows-console.cpp
@@ -879,6 +879,7 @@ int unittest4_c() {
 	allPass = allPass && tResult;
 	cout << "T1: isGameOver(gameState):" << PASS(tResult) << " Returns True when smithy, adventurer, cutpurse, gardens, village Supply = 0\n";
 
+	cout << "endTurn(gameState): Unit Tests 4 - all tests:" << PASS(allPass) << endl;
 	return allPass ? 0 : 1;
 }
 
@@ -917,13 +918,13 @@ int cardtest1_c() {
 	int player_i = whoseTurn(&G);
 
 	// Add Smithy card to the 5th Hand Position
-	G.hand[player_i][G.handCount[player_i]] = smithy; 
+	G.hand[player_i][G.handCount[player_i]] = smithy;
 	G.handCount[player_i]++; // hand should have 6 cards at this point
 
 	struct gameState G_original;
 	memcpy(&G_original, &G, sizeof(gameState));
 
- 	cardEffectSmithy(smithy, -1,  -1,  -1,&G, G.handCount[player_i] -1, 0); // the cardEffect signature is very odd to me... How coins are tracked is odd.
+	cardEffectSmithy(smithy, -1, -1, -1, &G, G.handCount[player_i] - 1, 0); // the cardEffect signature is very odd to me... How coins are tracked is odd.
 
 
 	// Smithy effect 
@@ -935,28 +936,28 @@ int cardtest1_c() {
 	allPass = allPass && tResult;
 	cout << "T1: Smithy Card Test:" << PASS(tResult) << " Returns true when handCount increases by 3\n";
 
-//		T2  Deck card count should decrease by 3.  (becuase initial state has 5 deck cards)
+	//		T2  Deck card count should decrease by 3.  (becuase initial state has 5 deck cards)
 	tResult = (G.deckCount == G_original.deckCount - 3);
 	allPass = allPass && tResult;
 	cout << "T2: Smithy Card Test:" << PASS(tResult) << " Returns true when deckCount decreases by 3\n";
 
-//		T3  Actions remain the same
+	//		T3  Actions remain the same
 	tResult = (G.numActions == G_original.numActions);
 	allPass = allPass && tResult;
 	cout << "T3: Smithy Card Test:" << PASS(tResult) << " Returns true when numActions remains the same\n";
 
-//		T4  Coins remain the same
+	//		T4  Coins remain the same
 	tResult = (G.coins == G_original.coins);
 	allPass = allPass && tResult;
 	cout << "T4: Smithy Card Test:" << PASS(tResult) << " Returns true when coins remains the same\n";
 
-//		T5  Buys remain the same 
-	tResult = (G.numBuys== G_original.numBuys);
+	//		T5  Buys remain the same 
+	tResult = (G.numBuys == G_original.numBuys);
 	allPass = allPass && tResult;
 	cout << "T5: Smithy Card Test:" << PASS(tResult) << " Returns true when numBuys remains the same\n";
 
 	//		T6  Played Cards Increases by 1
-	tResult = (G.playedCardCount == G_original.playedCardCount+1);
+	tResult = (G.playedCardCount == G_original.playedCardCount + 1);
 	allPass = allPass && tResult;
 	cout << "T6: Smithy Card Test:" << PASS(tResult) << " Returns true when playedCardCount increases by 1\n";
 
@@ -965,12 +966,149 @@ int cardtest1_c() {
 	int cardCountAfter = G_original.deckCount[player_i] + G_original.discardCount[player_i] + G_original.playedCardCount + G_original.handCount[player_i];
 	tResult = (cardCountBefore == cardCountAfter);
 	allPass = allPass && tResult;
-	cout << "T6: Smithy Card Test:" << PASS(tResult) << " Returns true when Total Cards owned by player remains the same.\n";
+	cout << "T7: Smithy Card Test:" << PASS(tResult) << " Returns true when Total Cards owned by player remains the same.\n";
 
+	cout << "Card Test 1- Smithy - all tests:" << PASS(allPass) << endl;
 	return allPass;
 };
+
 // Adventurer Car Unit Test
-int cardtest2_c() { return 1; };
+//    Adventurer card Draws cards from the deck until you reveal 2 Treasure Cards
+//		Testing Plan -- This card has a bit of integrated testing component to it becuase if there are insufficient
+//			treasure cards in the deck then the discard pile will have to be shuffled. There is guarented to be 
+//			a solution becuase the initial game state has 7 coppers in it.  (and I couldn't find a scenerio 
+//			where there wouldn't be enough Treasure cards in the discard and deck combined)
+//		
+//		Given this assertion that it is mathmatically impossible (no formal proof here) for there not to be 2
+//			Treasure cards in either the Deck or Deck and Discard combined.  We will focus on the state where
+//			there are 0,1,2,3 Treasure cards in the deck and the state where the First 2 draws are Treasure and 
+//			First 2 draws are not Treasure
+//		T1  Total Count of cards remain the same
+//		T2  Deck State permutations of k,k,T,T,T   = k means kindom card, T means Treasure Card  will be executed against the test battery
+//					for practicality, gold, silver and copper cards will be included but not every permuation of 
+//					the 3 different Treasure cards shall be considered.
+//				T2a		Total Player Owned Cards Remain the same
+//				T2b		handCount increases by 2
+//				T2c		Buys and Actions Remain the Same
+//				T2d		Coin Count before < Coin Count After
+//		
+int cardtest2_c() {
+	gameState G;
+	CardTestInitializeGame(&G); // initial state + 1 of each of the 5 supply cards in the deck
+	int player_i = whoseTurn(&G);
+
+	// Add Smithy card to the 5th Hand Position
+	G.hand[player_i][G.handCount[player_i]] = smithy;
+	G.handCount[player_i]++; // hand should have 6 cards at this point
+
+	struct gameState G_original;
+	memcpy(&G_original, &G, sizeof(gameState));
+
+	cardEffectAdventurer( &G); // the cardEffect signature is very odd to me... How coins are tracked is odd.
+
+
+	// Smithy effect 
+	bool allPass = true;
+	bool tResult = true;
+
+	//		T1  Total Count of cards remain the same
+	int cardCountBefore = G.deckCount[player_i] + G.discardCount[player_i] + G.playedCardCount + G.handCount[player_i];
+	int cardCountAfter = G_original.deckCount[player_i] + G_original.discardCount[player_i] + G_original.playedCardCount + G_original.handCount[player_i];
+	tResult = (cardCountBefore == cardCountAfter);
+	cout << "T1: Adventurer Card Test:" << PASS(tResult) << " Returns true when Total Cards owned by player remains the same.\n";
+
+	allPass = allPass && tResult;
+
+	// T2 with permutations
+	// In order to run the permutations we need to make sure there are 2 Treasure cards in the discard pile
+	G.discard[player_i][G.discardCount[player_i] - 1] = silver;
+	G.discardCount[player_i]++;
+	G.discard[player_i][G.discardCount[player_i] - 1] = gold;
+	G.discardCount[player_i]++;
+	int failedPermutations = 0;
+	int totalPermutations = 0;
+	cout << "Permutation No: " << "\tCoin Before" << "\tCoin After" <<  "\tValue Increase"
+		<< "\tCard Count Same"<<"\tHandCount+2"<<"\tBuys/Actions Same"<< endl; // 7 fields
+	for (int c1 = 0; c1 < 2; c1++) { // 1st Card
+		CARD card1 = c1 ? smithy : copper;
+		for (int c2 = 0; c2 < 2; c2++) { // 1st Card
+			CARD card2 = c2 ? smithy : gold;
+			for (int c3 = 0; c3 < 2; c3++) { // 1st Card
+				CARD card3 = c3 ? smithy : silver;
+				for (int c4 = 0; c4 < 2; c4++) { // 1st Card
+					CARD card4 = c4 ? smithy : copper;
+					for (int c5 = 0; c5 < 2; c4++) { // 1st Card
+						CARD card5 = c5 ? smithy : copper;
+						// all 32 Permutations of 5 cards covered here
+						totalPermutations++;
+						G.deck[player_i][0] = card1;
+						G.deck[player_i][1] = card2;
+						G.deck[player_i][2] = card3;
+						G.deck[player_i][3] = card4;
+						G.deck[player_i][4] = card5;
+						memcpy(&G_original, &G, sizeof(gameState));
+						cardEffectAdventurer(&G); // the cardEffect signature is very odd to me... How coins are tracked is odd.
+						// Check Total Card Count
+						//		T1-Repeat  Total Count of cards remain the same
+						int cardCountBefore = G.deckCount[player_i] + G.discardCount[player_i] + G.playedCardCount + G.handCount[player_i];
+						int cardCountAfter = G_original.deckCount[player_i] + G_original.discardCount[player_i] + G_original.playedCardCount + G_original.handCount[player_i];
+						bool tResult_TotalCardCount = (cardCountBefore == cardCountAfter);
+						bool tResult_HandCount_plus2 = (G.handCount[player_i] == G_original.handCount[player_i]);
+						bool tResult_Buys_Actions_same = (G.numBuys == G_original.numBuys) && (G.numActions == G_original.numActions);
+						// Check coint count
+						int coinCountBefore = 0;
+						int coinCountAfter = 0;
+						for (int j = 0; j < G.handCount[player_i]; j++)
+						{
+							switch (G.hand[player_i][j]) {
+								case copper:
+									coinCountAfter++;
+									break;
+								case silver:
+									coinCountAfter += 3;
+									break;
+								case gold:
+									coinCountAfter += 6;
+									break;
+							}
+
+						}
+						for (int j = 0; j < G_original.handCount[player_i]; j++)
+						{
+							switch (G_original.hand[player_i][j]) {
+								case copper:
+									coinCountBefore++;
+									break;
+								case silver:
+									coinCountBefore += 3;
+									break;
+								case gold:
+									coinCountBefore += 6;
+									break;
+							}
+						}
+						bool tResult_valueIncrease = (coinCountBefore < coinCountAfter);
+						allPass = allPass && tResult_valueIncrease &&tResult_TotalCardCount&&tResult_HandCount_plus2
+							&&tResult_Buys_Actions_same;
+						{ // print results: brack for code folding only
+							cout << ""<< totalPermutations << coinCountBefore <<  coinCountAfter;
+							PASS(tResult_valueIncrease);
+							PASS(tResult_TotalCardCount);
+							PASS(tResult_HandCount_plus2);
+							PASS(tResult_Buys_Actions_same);
+						}
+						cout << endl;
+
+					}
+				}
+			}
+		}
+	}
+
+
+	cout << "Card Test 2- Adventurer - all tests:" << PASS(allPass) << endl;
+	return allPass;
+};
 // cutpurse
 int cardtest3_c() { return 1; };
 // garden
